e56b843cb3d1cbf4d712ec5e58cceb871bddf3c5
mdasif8393 <md.asif.8393@gmail.com>
2025-02-14 23:56:18 +0600

    cpmplete

--- src/components/modules/auth/login/LoginForm.tsx
+++ src/components/modules/auth/login/LoginForm.tsx
@@ -10,21 +10,36 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { loginUser } from "@/services/AuthService";
+import { loginUser, reCaptchaTokenVerification } from "@/services/AuthService";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Link } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { loginSchema } from "./loginValidation";
 
-const LoginForm = () => {
+export default function LoginForm() {
   const form = useForm({
     resolver: zodResolver(loginSchema),
   });
 
+  const [reCaptchaStatus, setReCaptchaStatus] = useState(false);
+
   const {
     formState: { isSubmitting },
   } = form;
+
+  const handleReCaptcha = async (value: string | null) => {
+    try {
+      const res = await reCaptchaTokenVerification(value!);
+      if (res?.success) {
+        setReCaptchaStatus(true);
+      }
+    } catch (err: any) {
+      console.error(err);
+    }
+  };
 
   const onSubmit: SubmitHandler<FieldValues> = async (data) => {
     try {
@@ -77,8 +92,20 @@ const LoginForm = () => {
             )}
           />
 
-          <Button className="mt-5 w-full" type="submit">
-            {isSubmitting ? "Logging..." : "Login"}
+          <div className="flex mt-3 w-full">
+            <ReCAPTCHA
+              sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_CLIENT_KEY}
+              onChange={handleReCaptcha}
+              className="mx-auto"
+            />
+          </div>
+
+          <Button
+            disabled={reCaptchaStatus ? false : true}
+            type="submit"
+            className="mt-5 w-full"
+          >
+            {isSubmitting ? "Logging...." : "Login"}
           </Button>
         </form>
       </Form>
@@ -90,6 +117,4 @@ const LoginForm = () => {
       </p>
     </div>
   );
-};
-
-export default LoginForm;
+}
