97d4d5d2533d86b8fa07eda742d5bd1fd2822baa
mdasif8393 <md.asif.8393@gmail.com>
2025-02-14 21:34:23 +0600

    39-10: Creating a Context API to Access User Info in Client Side

--- src/app/(WithCommonLayout)/page.tsx
+++ src/app/(WithCommonLayout)/page.tsx
@@ -1,4 +1,9 @@
+"use client";
+import { useUser } from "@/context/UserContext";
+
 const HomePage = () => {
+  const user = useUser();
+  console.log(user);
   return (
     <div>
       <h1>Welcome to NextMart Home page</h1>
