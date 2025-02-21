7641c8abdefe6f591643ff48a592750c2fdcb7fc
mdasif8393 <md.asif.8393@gmail.com>
2025-02-21 23:14:18 +0600

    40-11: Modify Sidebar and Add Create Category Modal

--- src/app/(WithDashboardLayout)/user/page.tsx
+++
@@ -1,9 +0,0 @@
-const UserHomePage = () => {
-  return (
-    <div>
-      <h1>UserHomePage</h1>
-    </div>
-  );
-};
-
-export default UserHomePage;
---
+++ src/components/modules/shop/category/CreateCategoryModal.tsx
@@ -0,0 +1,92 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+const CreateCategoryModal = () => {
+  const form = useForm();
+  const {
+    formState: { isSubmitting },
+  } = form;
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    try {
+      console.log(data);
+    } catch (err: any) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Create Category</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create Product Category</DialogTitle>
+        </DialogHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input type="text" {...field} value={field.value || ""} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center justify-between mt-5">
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Description</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        className="h-36 w-72"
+                        {...field}
+                        value={field.value || ""}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <Button type="submit" className="mt-5 w-full">
+              {isSubmitting ? "Creating...." : "Create"}
+            </Button>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CreateCategoryModal;
